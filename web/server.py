eab9bc91e6c8f360fafa558a41bfaaa4ded24af8
omid <omedmm3@gmail.com>
2024-06-13 23:56:54 +0430

    added the base of backend and frontend

--- web/server.py
+++ web/server.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import random
 import json
 
 app = Flask(__name__)
 
-data = json.load(open("./data.json", "r"))
+data = json.load(open("./data.json", "r")) # load fal data from json file
 
 @app.route("/fal")
-def fal():
-    return jsonify(random.choice(data))
+def fal(): # returns a random fal as json response
+    return jsonify(random.choice(data)) 
 
-app.run(host="0.0.0.0", port=9999, debug=True)
+@app.route("/hx/fal")
+def hx_fal(): # returns a random fal as html response for htmx
+    fal = random.choice(data)
+    return render_template("fal.html", id=fal["id"], ghazal=fal["ghazal"], fal=fal["fal"])
+
+@app.route("/")
+def main(): # main page
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999, debug=True)
